874722012f07b536250d1d1487b33333c166ef3c
Kari Bronson <karibronson10@gmail.com>
2024-10-12 23:52:58 -0600

    initial commit - vuetify boiler plate with all in

---
+++ src/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <v-img cover src="/src/assets/kari-bronson-alphagetti-large.jpg"
+      ><template #placeholder>
+        <div class="d-flex align-center justify-center fill-height">
+          <v-progress-circular color="grey-lighten-4" indeterminate />
+        </div> </template
+    ></v-img>
+  </div>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
